6aa7ccc718d5fa995d7b6cfe90f36001a051c0af
arjun suresh <123arjunsuresh@gmail.com>
2017-02-05 15:23:31 +0530

    user playing arena updated

--- dashboard/img.php
+++ dashboard/img.php
@@ -1,14 +1,30 @@
 <?php
 include '../common/con.php';
 
+if(!isset($_GET['img'])){die();}
 $h=$_GET['img'];
+
 
 session_start();
 
 
 header("Content-type: image/png");
+$stmt = $mysqli->prepare('SELECT * from imageaccess where imagehash=?');
+$stmt->bind_param("s",$h);
+$stmt->execute();
+$res=$stmt->get_result();
+if(!$res->num_rows==1)
+{
+exit;
 
-readfile('../images/fblogin-btn.png');
+}
+$row=$res->fetch_assoc();
+
+
+
+
+
+readfile("../levelsmade/".$row['actualimage']);
 
 
 
@@ -16,7 +32,7 @@ readfile('../images/fblogin-btn.png');
 
 
 
-
+$stmt->close();
 
 
 
--- dashboard/index.php
+++ dashboard/index.php
@@ -1,17 +1,82 @@
 <?php
 include '../common/header.php';
-
+include '../common/con.php';
 session_start();
-
+$hash="";
 
 $userdata=$_SESSION['userData'];
+$email=$userdata['email'];
+if(!isset($userdata['isadmin']))
+{
+	$res=fetchquery("SELECT * from admins where email='$email';");
+	if($res->num_rows==1)
+	{
+	$_SESSION['userData']['isadmin']=1;
+
+	}
+	else{
+
+	$_SESSION['userData']['isadmin']=0;
+
+	}
+}
+$userid=$userdata['id'];
+$res=fetchquery("SELECT * from leadersboard where id=$userid;");
+if($res!=null)
+{
+	if($res->num_rows==1)
+	{
+	$row=$res->fetch_assoc();
+	$lvl=$row['lvlno'];
+
+	$res=fetchquery("SELECT * from levels where lvlno='$lvl';");
+	if($res!=null)
+	{
+		if($res->num_rows==1)
+		{ global $hash;
+			$rowlevel=$res->fetch_assoc();
+			
+		$id=$userdata['id'];
+			$hash=crypt($rowlevel['lvlimage'].$id,"saltitbro");
+			$ip=get_ip();
+
+		$actualimage=$rowlevel['lvlimage'];
+		fetchquery("INSERT into imageaccess values('$hash','$ip',$id,'$actualimage');");
+		
+
+		}
+		else{
 
 
-var_dump($userdata);
+
+
+		}
+
+		}
+		
+		else{
+		
+		echo "nolevel for user";
+		}
+
+
+	}
+	
+	
+	
+
+
+}
+else{
+
+echo 'error:leadersboard access';
+}
+
+
 ?>
 
 
-<img src="img.php?img=e"/>
+<img src="img.php?img=<?php echo $hash; ?>"/>
 
 
 
@@ -29,5 +94,6 @@ var_dump($userdata);
 include '../common/foot.php';
 
 ?>
+
 
 
