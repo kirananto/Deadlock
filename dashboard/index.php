f61fcf5d9e476be87316b52e16afc7f58b863bd0
Kiran Anto <kirananto@gmail.com>
2017-02-10 02:23:05 +0000

    Add footers to all main sites

--- dashboard/index.php
+++ dashboard/index.php
@@ -195,7 +195,7 @@ echo 'error:leadersboard access';
     </div>
 
 </section>
-<footer class="mbr-small-footer mbr-section mbr-section-nopadding" id="footer1-d" style="background-color: rgb(50, 50, 50); padding-top: 1.75rem; padding-bottom: 1.75rem;">
+<footer class="mbr-small-footer mbr-section mbr-section-nopadding" id="footer1-d" style="background-image: url(/assets/images/footer.jpg); padding-top: 1.75rem; padding-bottom: 1.75rem;">
     
     <div class="container">
         <p class="text-xs-center">Copyright (c) 2017 Deadlock.</p>
--- index.php
+++ index.php
@@ -181,7 +181,7 @@ $_SESSION['fbUser'] = $fbUser;
 
 </section>
 
-<footer class="mbr-small-footer mbr-section mbr-section-nopadding" id="footer1-2" style="background-color: rgb(50, 50, 50); padding-top: 1.75rem; padding-bottom: 1.75rem;">
+<footer class="mbr-small-footer mbr-section mbr-section-nopadding" id="footer1-d" style="background-image: url(/assets/images/footer.jpg); padding-top: 1.75rem; padding-bottom: 1.75rem;">
     
     <div class="container">
         <p class="text-xs-center">Copyright (c) 2017 Deadlock.</p>
